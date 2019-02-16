de8abe1116b300374c2d9f0d30116d803058a6cc
Jake Mathura <43418657+jakemathura@users.noreply.github.com>
2019-02-16 15:59:24 -0500

    Final version of FlowTimeCount

--- FlowTimeCount/FlowTimeCountReducer.java
+++ FlowTimeCount/FlowTimeCountReducer.java
@@ -1,3 +1,4 @@
+
 // reducer function for application to count the number of
 // times each unique IP address 4-tuple appears in an
 // adudump file.
@@ -8,21 +9,19 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.util.*;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class FlowTimeCountReducer
-  extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+public class FlowTimeCountReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
 
-  public void reduce(Text key, Iterable<DoubleWritable> values,
-      Context context)
-      throws IOException, InterruptedException {
+	public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+			throws IOException, InterruptedException {
 
-      long count = 0;
-      double timeTotal = 0;
+		long count = 0;
+		double timeTotal = 0;
 
-      // iterate through all the values (count == 1) with a common key
-      for (DoubleWritable value : values) {
-          timeTotal = timeTotal + value.get();
-      }
+		// iterate through all the values (count == 1) with a common key
+		for (DoubleWritable value : values) {
+			timeTotal = timeTotal + value.get();
+		}
 
-      context.write(key, new DoubleWritable(timeTotal));
-  }
+		context.write(key, new DoubleWritable(timeTotal));
+	}
 }
